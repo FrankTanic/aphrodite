aa899eeb6f6c7da14e6137dd4d0513bf92e4c81a
Frank van Driel <frank.vandriel@hotmail.com>
2014-11-16 21:26:47 +0100

    Added a Front migration file

---
+++ Aphrodite/Aphrodite.Front/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Aphrodite.Front.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Aphrodite.Front.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Aphrodite.Front.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
