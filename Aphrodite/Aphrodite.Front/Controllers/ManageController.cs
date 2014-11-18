83e13d2f9ea84ec9983deb8decf0d2a654d613f6
litsher <litsher@gmail.com>
2014-11-18 11:22:49 +0100

    Newer version of Manage

--- Aphrodite/Aphrodite.Front/Controllers/ManageController.cs
+++ Aphrodite/Aphrodite.Front/Controllers/ManageController.cs
@@ -51,8 +51,12 @@ namespace Aphrodite.Front.Controllers
             var model = new IndexViewModel
             {
                 Email = UserManager.GetEmail(User.Identity.GetUserId()),
+                Gender = UserManager.
                 HasPassword = HasPassword(),
-
+                PhoneNumber = await UserManager.GetPhoneNumberAsync(User.Identity.GetUserId()),
+                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(User.Identity.GetUserId()),
+                Logins = await UserManager.GetLoginsAsync(User.Identity.GetUserId()),
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(User.Identity.GetUserId())
             };
             return View(model);
         }
