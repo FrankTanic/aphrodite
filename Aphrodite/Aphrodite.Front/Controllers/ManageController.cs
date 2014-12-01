b796b05b35b20b4ac4a76b156d97945710148a95
Frank van Driel <frank.vandriel@hotmail.com>
2014-12-01 14:34:42 +0100

    removed double references

--- Aphrodite/Aphrodite.Front/Controllers/ManageController.cs
+++ Aphrodite/Aphrodite.Front/Controllers/ManageController.cs
@@ -11,8 +11,6 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web;
-using System.Web.Mvc;
-using Aphrodite.Front.Models;
 
 namespace Aphrodite.Front.Controllers
 {
