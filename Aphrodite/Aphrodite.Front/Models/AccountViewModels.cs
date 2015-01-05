cdfe8e8a2dd118df6e52c4597dbea540e6fb39f6
Frank van Driel <frank.vandriel@hotmail.com>
2015-01-05 12:56:43 +0100

    removed unnecessary identity code

--- Aphrodite/Aphrodite.Front/Models/AccountViewModels.cs
+++ Aphrodite/Aphrodite.Front/Models/AccountViewModels.cs
@@ -6,42 +6,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Aphrodite.Front.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
-
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
-
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
-
-    public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
-
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
-
-        [Display(Name = "Remember this browser?")]
-        public bool RememberBrowser { get; set; }
-
-        public bool RememberMe { get; set; }
-    }
-
     public class ForgotViewModel
     {
         [Required]
@@ -101,7 +65,7 @@ namespace Aphrodite.Front.Models
             {
                 try
                 {
-                    return new DateTime(BirthDayYear, BirthDayMonth, BirthDayDay);
+                    return new DateTime(BirthDayYear, BirthDayMonth, BirthDayDay); 
                 }
                 catch
                 {
@@ -188,7 +152,6 @@ namespace Aphrodite.Front.Models
         }
 
     }
-
 
     public class ResetPasswordViewModel
     {
