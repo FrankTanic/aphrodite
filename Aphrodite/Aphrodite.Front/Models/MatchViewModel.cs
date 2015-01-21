7390faa439200155bec5c478ddb370bc04ff9d3c
litsher <litsher@gmail.com>
2015-01-21 09:35:17 +0100

    Added photo of user.

--- Aphrodite/Aphrodite.Front/Models/MatchViewModel.cs
+++ Aphrodite/Aphrodite.Front/Models/MatchViewModel.cs
@@ -18,5 +18,7 @@ namespace Aphrodite.Front.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string File { get; set; }
+
+        public UserPhoto photo { get; set; }
     }
 }
